da1848f9ca1138ffda664e9fbc5a5ef47d6bfc5b
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2024-02-14 23:23:26 +0600

    added new test

--- src/test/java/ru/netology/web/cardDeliveryTest.java
+++ src/test/java/ru/netology/web/cardDeliveryTest.java
@@ -1,16 +1,14 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -24,7 +22,7 @@ public class cardDeliveryTest {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Москва");
         String planningDate = generateDate(4,"dd.MM.yyyy");
-        $("[data-test-id='date'] input").sendKeys((Keys.chord(Keys.SHIFT,Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Анна-Мария");
         $("[data-test-id='phone'] input").setValue("+78001675812");
